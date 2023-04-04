6b371371d398d82f6675ff6eef6a5195862bcdf7
dwivalentinaf <113223511+dwivalentinaf@users.noreply.github.com>
2023-04-04 07:39:03 -0700

    Add files via upload

---
+++ js_edit.js
@@ -0,0 +1,13 @@
+/*untuk membuat scroll up*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-Up')
+}
+
+
+const menuKlik = document.querySelector(".menu-klik input");
+const header = document.querySelector(" header ul");
+
+menuKlik.addEventListener ("clik", function () {
+    header.classList.klik("slide");
+});
+
